6e943364b2d6272b41ce12f34f7b6e935b2ff9ea
Saxavet.qocayev <saxavet.qocayev@cybernet.az>
2026-02-09 11:08:06 +0400

    app-success test

--- react-testing-library/src/App.test.tsx
+++ react-testing-library/src/App.test.tsx
@@ -4,7 +4,7 @@ import { describe, test, expect } from "vitest";
 import "@testing-library/jest-dom/vitest";
 
 describe("App component", () => {
-  test.only("renders hello text", () => {
+  test("renders hello text", () => {
     render(<App />);
     expect(screen.getByText(/react/i)).toBeInTheDocument();
   });
--- react-testing-library/src/components/Success/Success.test.tsx
+++ react-testing-library/src/components/Success/Success.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Success from ".";
 import { expect, test } from "vitest";
 
-test.skip("renders succdess text", () => {
+test("renders succdess text", () => {
   render(<Success country="USA" />);
   expect(screen.getByText(/USA/i)).toBeInTheDocument();
 });
