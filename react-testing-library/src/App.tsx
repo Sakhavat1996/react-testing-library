be1f30774e4acaad67caef7651172ab31a4c5678
Saxavet.qocayev <saxavet.qocayev@cybernet.az>
2026-02-10 17:06:54 +0400

    getByRole

--- react-testing-library/src/App.tsx
+++ react-testing-library/src/App.tsx
@@ -1,19 +1,10 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Application } from "./components/application";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Application />
       <footer>Footer App Component</footer>
     </>
   );
