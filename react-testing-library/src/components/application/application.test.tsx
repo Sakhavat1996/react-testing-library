5ea31dbc8c372d12bd4e91d41210feb67814cd68
Saxavet.qocayev <saxavet.qocayev@cybernet.az>
2026-02-12 16:52:43 +0400

    getByRole options

--- react-testing-library/src/components/application/application.test.tsx
+++ react-testing-library/src/components/application/application.test.tsx
@@ -5,8 +5,24 @@ import { Application } from ".";
 describe("Application", () => {
   test("renders correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox");
+
+    const pageHeading = screen.getByRole("heading", {
+      // name: "Job Application Form",
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      // name: "heading2",
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    const nameElement = screen.getByRole("textbox", { name: "Name" });
     expect(nameElement).toBeInTheDocument();
+
+    const name2Element = screen.getByRole("textbox", { name: "Name2" });
+    expect(name2Element).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
--- react-testing-library/src/components/application/index.tsx
+++ react-testing-library/src/components/application/index.tsx
@@ -1,28 +1,36 @@
 export const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="">Job Location</label>
-        <select name="" id="job-location">
-          <option value="">Select Country</option>
-          <option value="usa">USA</option>
-          <option value="uk">UK</option>
-          <option value="can">Canada</option>
-          <option value="fr">France</option>
-          <option value="nor">Norway</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="terms">
-          <input type="checkbox" id="terms" /> I agree to the terms and
-          conditions
-        </label>
-      </div>
-      <button>Submit</button>
-    </form>
+    <>
+      <h1>Job Application Form</h1>
+      <h2>heading2</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div>
+          <label htmlFor="textarea">Name2</label>
+          <textarea id="textarea" />
+        </div>
+        <div>
+          <label htmlFor="">Job Location</label>
+          <select name="" id="job-location">
+            <option value="">Select Country</option>
+            <option value="usa">USA</option>
+            <option value="uk">UK</option>
+            <option value="can">Canada</option>
+            <option value="fr">France</option>
+            <option value="nor">Norway</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="terms">
+            <input type="checkbox" id="terms" /> I agree to the terms and
+            conditions
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
   );
 };
